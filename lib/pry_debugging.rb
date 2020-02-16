79204b9759a32feaa4dc5d9947264c2d4ffd9597
Lesly Jean-Baptiste <lesjba56@yahoo.com>
2020-02-16 19:06:23 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,5 +1,15 @@
+<<<<<<< HEAD
 def plus_two(num)
 	num + 2
  
 	
 end
+=======
+require "pry"
+require "require./lib/pry_debugging.rb"
+
+def plus_two(3)
+  num + 2
+  
+end
+>>>>>>> d50d2a6766df8461e936e2f0911a74de6d308660
